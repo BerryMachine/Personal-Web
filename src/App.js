678dcb1fbcea3ae1cbfc758f268c7297d9f67837
Jason He <117488527+OhmzWithBerries@users.noreply.github.com>
2024-04-17 17:32:25 -0400

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Title'>
+        <h2 class="hithere">This is</h2>
+        <h1 class="name">JASON HE</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
