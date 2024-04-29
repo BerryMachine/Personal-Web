eeeaab994cad748abf18310be25211e78cbd0bf9
Jason He <jasonryan.he@gmail.com>
2024-04-28 23:08:04 -0400

    commit4

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <div className="App">
       <div className='Title'>
-        <h2 class="hithere">This is</h2>
+        <h2 class="hithere">Hello, I am</h2>
         <h1 class="name">JASON HE</h1>
       </div>
       <div className="Portrait">
+        <h3 class="intro_title">This is Jason</h3>
       </div>
     </div>
   );
